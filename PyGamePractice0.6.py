38b8d17dd9a7570ef8c47d51facda8d1812a3bd0
Baron R <70594492+s11275880@users.noreply.github.com>
2021-11-29 09:36:05 -0500

    Add files via upload
    Drawing circle and ellipses.

---
+++ PyGamePractice0.6.py
@@ -0,0 +1,45 @@
+# PyGame Practice, Ryan Kelley, 11/29/21 9:35am, v0.6
+
+from _typeshed import ReadableBuffer
+import pygame, sys
+from pygame.locals import *
+
+# start PyGame
+pygame.init()
+
+#Create game window
+windowSurface = pygame.display.set_mode((500,400),0,32)
+pygame.display.set_caption("Hello, World")
+
+# Set Color Values
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0 , 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+# Setup Fonts
+basicFont = pygame.font.SysFont(None, 48)
+
+# Setup Text 
+text = basicFont.render('Hello, world', True, WHITE, BLUE)
+textRect = text.get_rect()
+textRect.centerx = windowSurface.get_rect().centerx
+textRect.centery = windowSurface.get_rect().centery
+
+# Draw background onto windows surface. 
+windowSurface.fill(WHITE)
+
+# Draw a green polygon
+pygame.draw.polygon(windowSurface, GREEN, ((146, 0), (291, 106), (236, 277), (56, 277), (0, 106)))
+
+# Draw blue lines on the windowSurface
+pygame.draw.line(windowSurface, BLUE, (120), 60), (120, 60), 4)
+pygame.draw.line(windowSurface, BLUE, (120, 60), (60, 60))
+pygame.draw.line(windowSurface, BLUE, (60, 120), (120, 120), 4)
+
+# Draw a circle.
+pygame.draw.circle(windowSurface, BLUE, (300, 50), 20, 0)
+
+# Draw an ellipse
+pygame.draw.ellipse(windowSurface, RED, (300, 250, 40, 80), 1)
